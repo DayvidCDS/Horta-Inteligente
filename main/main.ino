ad75c72824c823194a140888dc880c50dbb2591a
dayvidcds <dayvid.clementino@gmail.com>
2017-10-14 21:18:07 -0300

    adicionei o arquivo com funções para leitura de botões e adicionei também um método que conta o tempo em segundos que o arduino está ativo

--- main/main.ino
+++ main/main.ino
@@ -3,11 +3,12 @@
 #include "rele.cpp";
 #include "sensor_de_luz.cpp";
 #include "sensor_de_temp_umi.cpp";
+#include "botao.cpp";
 
 // Criando variáveis para o "contador global"
 unsigned long tempoPassado = 0;
 unsigned long tempoInicial = 0;
-unsigned long tempoDeAmostra = 30;
+const unsigned long tempoDeAmostra = 30;
 
 // Definições gerais
 #define PIN_BOMBA 2
@@ -24,13 +25,24 @@ TempUmi dht(dhtParam); // Passando sensor DHT como parâmetro para o nosso objet
 LiquidCrystal_I2C lcdParam(0x3F, 16, 2); // Instanciando Display LCD
 Display lcd(lcdParam); // Passando como parâmetro o objeto lcdParam no objeto lcd
 
+// cabeçalho das funções criadas neste arquivo
+void calcularTempo(long *, long *, const long);
+
 void setup() {
   lcd.iniciar();
-  dht.begin();
+  dhtParam.begin();
   pinMode(PIN_BOMBA, OUTPUT);
 }
 
 void loop() {
   // Aqui só implementa a lógica do sistema
+  calcularTempo(&tempoInicial, &tempoInicial, tempoDeAmostra);
+}
+
+void calcularTempo(long *tempoPassado, long *tempoInicial, const long tempoAmostra) { // Função que calcula tempo que o arduino está ligado (Aconselhasse utilizar direto no loop)
+  *tempoPassado = millis() - (*tempoInicial); // mensurando o tempo
+  if (*tempoPassado > (tempoAmostra*1000)) {
+    *tempoInicial = millis();
+  }
 }
 
